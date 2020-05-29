a70d1d2fa21f001aaca6a26d7f4537886cc9513e
nkmathew <kipkoechmathew@gmail.com>
2020-05-29 10:40:04 +0300

    Implement theme generator function

--- Chromium/content-script.js
+++ Chromium/content-script.js
@@ -38,4 +38,85 @@ function darkTheme() {
   console.log('Done themeing...');
 }
 
-function generateTheme() {}
+function definedProperties() {
+  let cssrules;
+  let defined = [];
+  for (var sheet of document.styleSheets) {
+    try {
+      cssrules = sheet.cssRules;
+      console.log('Working: ', sheet.href);
+    } catch (error) {
+      console.log('Error', sheet.href);
+      continue;
+    }
+    for (var rule of cssrules) {
+      if (rule.style && (rule.style.color || rule.style.backgroundColor)) {
+        let selector = rule.selectorText;
+        defined.push(...selector.split(/,\s*/));
+      }
+    }
+  }
+  return defined;
+}
+
+function joinSelectors(res) {
+  res = res
+    .map((sel) => [sel, document.querySelector(sel)])
+    .filter(([sel, node]) => node && node.tagName != 'A')
+    .map(([sel, node]) => sel);
+  res = [...new Set(res)];
+  res.sort((a, b) => {
+    a = ('~' + a).replace(/^(~[.#])/, '');
+    b = ('~' + b).replace(/^(~[.#])/, '');
+    return a < b ? -1 : a > b ? 1 : 0;
+  });
+  return res.join(',\n');
+}
+
+CSS = `
+
+{
+  background: #191919 !important;
+  color: #ccc;
+  font-family: "Segoe UI";
+  line-height: 1.3;
+}
+
+a {
+  color: #4db2ec;
+}
+
+`;
+
+function copyText(text) {
+  let input = document.querySelector('#copy-area');
+  if (!input) {
+    input = document.createElement('textarea');
+    input.id = 'copy-area';
+    document.body.appendChild(input);
+  }
+  input.style.position = 'fixed';
+  input.style.top = '5%';
+  input.style.display = 'block';
+  input.style.marginLeft = '35%';
+  input.style.width = '500px';
+  input.style.height = '500px';
+  input.value = text;
+  setTimeout(() => {
+    input.select();
+  }, 500);
+  input.addEventListener('click', (event) => {
+    document.execCommand('copy');
+    setTimeout(() => {
+      event.target.style.display = 'none';
+    });
+  });
+}
+
+function generateTheme() {
+  res = definedProperties();
+  res = joinSelectors(res);
+  res += CSS;
+  console.log(res);
+  copyText(res);
+}
