14339d4d90439011072d9bd64b3d1f23e44d2c8b
nkmathew <kipkoechmathew@gmail.com>
2020-06-18 16:46:22 +0300

    Add button for copying text in pre tags

--- Chromium/content-script.js
+++ Chromium/content-script.js
@@ -7,10 +7,15 @@ chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
     document.querySelector('html').style.filter = request.action;
   } else if (request.action == 'start-zapper') {
     Zapper();
+    dqs('#dtg-delete').click();
   }
 });
 
 // }}============================================================================={{
+
+KEYWORD  = '#8FEB08';
+STRING   = 'Khaki';
+CONSTANT = 'FFA0A0';
 
 const dqs = (selector) => document.querySelector(selector);
 const dqsa = (selector) => document.querySelectorAll(selector);
@@ -50,7 +55,42 @@ a.dtg-externalLink:hover {
   background-position: top right !important;
 }
 
+button.dtg-copy-button {
+  background: #3f4d5f;
+  border: 0;
+  padding-top: 3px;
+  padding-bottom: 3px;
+  border-radius: 3px;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  font-size: 10px;
+  font-family: monospace;
+  color: #ccc;
+  font-weight: bold;
+}
+
 `;
+
+
+function makeCopyButton() {
+  let button = document.createElement('button');
+  button.classList.add('dtg-copy-button');
+  button.innerText = 'Copy';
+  button.addEventListener('click', (event) => {
+    let preText = event.target.closest('pre').innerText;
+    preText = preText.replace(/\n+Copy\n?$/g, '');
+    copyText(preText, true);
+  });
+  return button;
+}
+
+function hasAnyClass(node, classList) {
+  if (typeof classList == 'string') {
+    classList = [classList];
+  }
+  return [...node.classList].filter(item => classList.includes(item)).length;
+}
 
 function makePageDark() {
   addStylesheet(CSS);
@@ -76,6 +116,7 @@ function makePageDark() {
       node.style.color = '#d1d1d1e6';
     }
     if (inLink) {
+      node.style.background = '#191919';
       node.classList.add('dtg-linkHover');
       if (inParagraph) {
         node.classList.add('dtg-externalLink');
@@ -97,21 +138,27 @@ function makePageDark() {
     } else if (inCode) {
       node.style.fontFamily = 'Consolas';
       node.style.background = '#2A3340';
+      node.style.backgroundColor = '#2A3340';
       node.style.fontSize = '13px';
       node.style.lineHeight = '17px';
       node.style.color = '#ccc';
       if (node.classList.contains('hljs-number')) {
-        node.style.color = '#FFA0A0';
+        node.style.color = CONSTANT;
       } else if (node.classList.contains('hljs-attr')) {
         node.style.color = '#ccc';
         if (/["'].+["']/.test(node.innerText)) {
           node.style.color = '#ff9800';
         }
+      } else if (hasAnyClass(node, ['k'])) {
+        node.style.color = KEYWORD;
       } else if (node.classList.contains('hljs-string')) {
-        node.style.color = 'khaki';
+        node.style.color = STRING;
       } else {
-        if (tag == 'span' && /["'].+["']/.test(node.innerText)) {
+        let text = node.innerText;
+        if (tag == 'span' && /["'].+["']/.test(text)) {
           node.style.color = '#ff9800';
+        } else if (tag == 'span' && /^[\d.]*\d$/.test(text)) {
+          node.style.color =  CONSTANT;
         }
       }
     } else if (['p', 'li'].includes(tag)) {
@@ -119,11 +166,14 @@ function makePageDark() {
       node.style.fontSize = '16px';
       node.style.lineHeight = '28px';
     }
+    if (tag == 'pre') {
+      node.style.position = 'relative';
+      node.appendChild(makeCopyButton());
+    }
     node.style.borderColor = '#555';
     node.style.borderRadius = '5px';
     node.style.boxShadow = 'none';
   });
-  console.log('Done themeing...');
 }
 
 function definedProperties() {
@@ -165,10 +215,21 @@ ROOT = `
 
 :root {
 
+  --bg           : #191919;
+  --bg-1         : #000000;
+  --bg-2         : #121212;
+  --bg-3         : #222222;
+  --bg-3024      : #090300;
+  --bg-4         : #262626;
+  --bg-5         : #2E2E2E;
+  --bg-6         : #23232D;
+  --bg-7         : #42424C;
+  --bg-alt       : #121212;
   --bg-anki      : #2F2F31;
   --bg-card      : #343A40;
   --bg-imgur     : #141518;
   --bg-pre       : #2A3340;
+  --bg-pre1      : #24292E;
   --bg-quote     : #2B29298C;
   --blue         : #1A73E8;
   --bold         : #F2D297;
@@ -180,22 +241,17 @@ ROOT = `
   --border-2     : #FFFFFF1A;
   --border-3     : #333333;
   --border-quote : #565680;
+  --btn-blue     : #378AD3;
   --btn-like     : #EF5466;
   --btn-shadow   : 0 0 5px #0F0F0F;
+  --btn-signup   : #009966;
   --fg           : #CCCCCC;
   --fg-1         : #FFFFFF;
   --fg-2         : #F5DEB3;
   --fg-3         : #B3B3B3;
+  --fg-4         : #d3d3d3;
+  --fg-5         : #ffffffbf;
   --fg-input     : #FFFFFFE0;
-  --gray         : #191919;
-  --gray-1       : #000000;
-  --gray-2       : #121212;
-  --gray-3       : #222222;
-  --gray-4       : #262626;
-  --gray-5       : #2E2E2E;
-  --gray-6       : #23232D;
-  --gray-7       : #42424C;
-  --gray-alt     : #121212;
   --green        : #228822;
   --header       : #BDB76B;
   --header-1     : #FF9800DE;
@@ -217,9 +273,10 @@ DARKCSS = `
   color: #ccc !important;
   font-family: "Segoe UI" !important;
   line-height: 28px;
-  border-color: #555;
-  /* font-weight: */
+  border-color: #555 !important;
+  border-radius: 3px !important;
   /* font-size: 16px; */
+  /* font-weight: */
 }
 
 a {
@@ -279,9 +336,35 @@ strong {
   width: 100%;
 }
 
+pre.prettyprint {
+  padding: 10px;
+  font-family: monospace;
+  background: var(--bg-pre) !important;
+  border-color: #555 !important;
+  border-radius: 5px;
+  font-size: 14px;
+  line-height: normal;
+}
+
+pre .com {
+  color: var(--pre-comment);
+}
+
+pre .kwd {
+  color: var(--pre-keyword);
+}
+
+pre .pln {
+  color: var(--fg);
+}
+
+pre .str {
+  color: var(--pre-string);
+}
+
 `;
 
-function copyText(text) {
+function copyText(text, hidden) {
   let input = document.querySelector('#copy-area');
   if (!input) {
     input = document.createElement('textarea');
@@ -292,18 +375,19 @@ function copyText(text) {
   input.style.top = '5%';
   input.style.display = 'block';
   input.style.marginLeft = '35%';
-  input.style.width = '500px';
-  input.style.height = '500px';
-  input.style.zIndex = '1e9';
+  input.style.width = '100px';
+  input.style.height = '100px';
+  input.style.zIndex = '99999999';
+  input.style.cursor = 'pointer';
   input.value = text;
   setTimeout(() => {
     input.select();
-  }, 500);
+  }, 1e3);
   input.addEventListener('click', (event) => {
     document.execCommand('copy');
     setTimeout(() => {
       event.target.style.display = 'none';
-    });
+    }, 2e3);
   });
 }
 
