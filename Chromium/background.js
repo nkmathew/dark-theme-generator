e13b2c407c9260e83a7651fb2ac8229133d6c1d8
nkmathew <kipkoechmathew@gmail.com>
2020-06-20 11:27:49 +0300

    Add zapper button to right click menu

--- Chromium/background.js
+++ Chromium/background.js
@@ -62,8 +62,8 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.create({
   id: 'btn-zapper',
-  title: 'Start Zapper',
-  contexts: ['browser_action'],
+  title: '→ Start Zapper',
+  contexts: ['all'],
   onclick: function () {
     Chrome.getCurrentTab().then((currTab) => {
       chrome.tabs.sendMessage(currTab.id, { action: 'start-zapper' });
